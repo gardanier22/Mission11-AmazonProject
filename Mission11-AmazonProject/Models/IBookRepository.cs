62fabe7ebcb20149931b31e3208ab42cc45c8d21
gardanier22 <agardanier22@gmail.com>
2024-03-22 10:33:16 -0600

    Add project files.

---
+++ Mission11-AmazonProject/Models/IBookRepository.cs
@@ -0,0 +1,8 @@
+﻿namespace Mission11_AmazonProject.Models
+{
+    public interface IBookRepository
+    {
+        // double check to make sure that its calles 'Books' right here 
+        public IQueryable<Book> Books { get; }
+    }
+}
